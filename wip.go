429ea78e530c61a23ef642a94228be23fd14d7cd
Chris Harrington <ironiridis@gmail.com>
2016-03-06 16:10:14 -0600

    apparently 'block forever' means exit immediately

--- wip.go
+++ wip.go
@@ -1,15 +1,16 @@
 package main
 
 import "fmt"
+import "time"
 
 func main() {
 	// set up terminal interface
-	fmt.Print("\x1b[9;0]") // blank 0
-	fmt.Print("\x1b[14;0]") // powerdown 0
-	fmt.Print("\x1b[41m") // red background (for troubleshooting)
+	fmt.Print("\x1b[9;0]")         // blank 0
+	fmt.Print("\x1b[14;0]")        // powerdown 0
+	fmt.Print("\x1b[41m")          // red background (for troubleshooting)
 	fmt.Print("\x1b[?25l\x1b[?1c") // disable cursor
-	fmt.Print("\x1b[8]") // store defaults
-	fmt.Print("\x1b[H\x1b[J") // clear screen
+	fmt.Print("\x1b[8]")           // store defaults
+	fmt.Print("\x1b[H\x1b[J")      // clear screen
 
 	b, err := decodeImageBGR8("/home/charrington/1.jpg")
 	if err != nil {
@@ -19,8 +20,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	
-	var c chan string
-	c <- "wait forever"
-}
 
+	for {
+		<-time.After(1 * time.Hour)
+	}
+}
