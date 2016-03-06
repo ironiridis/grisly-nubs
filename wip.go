45579ce3734a0acc69c74875710fc0373fedec9d
Chris Harrington <ironiridis@gmail.com>
2016-03-06 16:00:12 -0600

    terminal sequences and wait forever

--- wip.go
+++ wip.go
@@ -1,6 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
+	// set up terminal interface
+	fmt.Print("\x1b[9;0]") // blank 0
+	fmt.Print("\x1b[14;0]") // powerdown 0
+	fmt.Print("\x1b[41m") // red background (for troubleshooting)
+	fmt.Print("\x1b[?25l\1b[?1c") // disable cursor
+	fmt.Print("\x1b[8]") // store defaults
+	fmt.Print("\x1b[H\x1b[J") // clear screen
+
 	b, err := decodeImageBGR8("/home/charrington/1.jpg")
 	if err != nil {
 		panic(err)
@@ -9,4 +19,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	
+	var c chan string
+	c <- "wait forever"
 }
+
