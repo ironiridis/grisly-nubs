1a464ee726fa80ac4fcea7e8d045ade4c35bf3dd
Chris Harrington <ironiridis@gmail.com>
2016-03-06 14:40:42 -0600

    refactor to correctly leverage golang image pkg

--- decoders.go
+++ decoders.go
@@ -1,42 +1,27 @@
 package main
 
-import "image/png"
-import "image/jpeg"
+import "image"
+import _ "image/png"
+import _ "image/jpeg"
 import "bytes"
 import "os"
 
-func decodePNG(path string) (bytes.Buffer, error) {
+func decodeImageBGR8(path string) (*bytes.Buffer, error) {
 	fp, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
 	defer fp.Close()
+	im, _, err := image.Decode(fp)
 	if err != nil {
 		return nil, err
 	}
-	im, err := png.Decode(fp)
-	if err != nil {
-		return nil, err
-	}
-	return imageToBGRBuffer(im)
-}
-
-func decodeJPEG(path string) (bytes.Buffer, error) {
-	fp, err := os.Open(path)
-	defer fp.Close()
-	if err != nil {
-		return nil, err
-	}
-	im, err := jpeg.Decode(fp)
-	if err != nil {
-		return nil, err
-	}
-	return imageToBGRBuffer(im)
-}
-
-func imageToBGRBuffer(im *image.Image) (buf bytes.Buffer, err error) {
+	buf := new(bytes.Buffer)
 	for y := im.Bounds().Min.Y; y < im.Bounds().Max.Y; y++ {
 		for x := im.Bounds().Min.X; x < im.Bounds().Max.X; x++ {
 			pr, pg, pb, _ := im.At(x, y).RGBA()
 			buf.Write([]byte{byte(pb >> 8), byte(pg >> 8), byte(pr >> 8)})
 		}
 	}
-	return
+	return buf, nil
 }
